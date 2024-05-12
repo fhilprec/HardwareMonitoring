9017a1c06412d0c384b9125d9872de82300ebc15
fhilprec <fhilprec@googlemail.com>
2024-05-12 19:22:59 +0200

    CSV writer

--- test.cpp
+++ test.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include "PerfEvent.hpp"
+#include "CSVWriter.h"
+
+//sysctl -w kernel.perf_event_paranoid=-1
+//g++ test.cpp ; ./a.out ; rm a.out;
 
 int main(){
-    int n = 10;
-
+    int a = 7;
+    int n = 10000;
     PerfEvent e;
     e.startCounters();
-
-    for(int i = 0; i < n; i++){
-        printf("Hi\n");
-    }
-
+    for (int i=0; i<n; i++) // this code will be measured
+    a = a * a;
     e.stopCounters();
-    e.printReport(std::cout, n);
+    e.printReport(std::cout, n); // use n as scale factor
     std::cout << std::endl;
-    
+
+    CSVWriter csv;
+    csv.newRow() << "this" << "is" << "the" << "first" << "row";
+    csv.newRow() << "this" << "is" << "the" << "second" << "row";
+    csv.writeToFile("foobar.csv");
     return 0;
 }
