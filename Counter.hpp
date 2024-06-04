6059d9b038eb3a0832bfb0564ae9ae33b778fe8f
Florian Hilprecht <fhilprecht@c02.lab.dm.informatik.tu-darmstadt.de>
2024-06-04 20:16:16 +0200

    a

---
+++ Counter.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "Device.hpp"
+
+
+
+class Counter {
+    public:
+        Counter(){
+
+        }
+        void start(){}
+        void stop(){}
+    private:
+        std::vector<Device> vec;
+
+        /*buffer.append((vec[0].getdata());*/
+};
+
---
+++ Device.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+
+
+struct Metric{
+    bool isPolling = false;
+    std::string name;
+};
+
+
+class Device {
+    public:
+        Device(){
+
+        }
+        std::vector<std::pair<Metric, double>> getData(bool isPolling = false);
+        virtual double fetchMetric(Metric metric) = 0;
+
+    private:
+        std::vector<Metric> allowed_metrics;
+};   
