1630f70628b2fa9d52079215d009294c484a7498
florian.hilprecht@stud.tu-darmstadt.de <florian.hilprecht@stud.tu-darmstadt.de>
2024-06-10 14:27:13 +0200

    a

--- Device.hpp
+++ Device.hpp
@@ -3,6 +3,20 @@
 #include <linux/perf_event.h>
 #include <sys/syscall.h>
 
+#include <chrono>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <asm/unistd.h>
+#include <linux/perf_event.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
 
 
 struct Metric{
@@ -60,12 +74,17 @@ public:
     }
         if (metric.name == "instructions") {
             struct perf_event_attr pe;
-            pe.type = PERF_TYPE_HARDWARE;
-            pe.config = PERF_COUNT_HW_INSTRUCTIONS;
-            pe.sample_period = 1000;
-            pe.sample_type = PERF_SAMPLE_RAW;
-            pe.sample_regs_user = PERF_SAMPLE_REGS_USER | PERF_SAMPLE_REGS_INTR;
-            pe.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING | PERF_FORMAT_ID;
+            memset(&pe, 0, sizeof(struct perf_event_attr));
+            pe.type = static_cast<uint32_t>(PERF_TYPE_HARDWARE);
+            pe.size = sizeof(struct perf_event_attr);
+            pe.config = PERF_COUNT_HW_CPU_CYCLES;
+            pe.disabled = true;
+            pe.inherit = 1;
+            pe.inherit_stat = 0;
+            pe.exclude_user = !(0b111 & 0b1);
+            pe.exclude_kernel = !(0b111 & 0b10);
+            pe.exclude_hv = !(0b111 & 0b100);
+            pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
             auto ans =syscall(__NR_perf_event_open, &pe, -1, -1, -1, 0);
             //also read our errno
             if (errno != 0) {
