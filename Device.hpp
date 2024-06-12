facd4a5187eec661f3f67aae026c091d665ded4a
florian.hilprecht@stud.tu-darmstadt.de <florian.hilprecht@stud.tu-darmstadt.de>
2024-06-12 12:46:16 +0200

    use witch case to look for appropriate metric

--- Device.hpp
+++ Device.hpp
@@ -19,21 +19,51 @@
 #include <unistd.h>
 
 
-
-struct Metric{
-    Metric(bool isPolling, std::string name) : isPolling(isPolling), name(name) {}
-    bool isPolling = false;
-    std::string name;
+enum Sampler{
+    /* Reads at the end of the process */
+    ONE_SHOT,
+    /* Reads both at the start and the end of the process and returns the difference */
+    TWO_SHOT,
+    /* Reads at an interval */
+    POLLING
 };
+
+struct Metric {
+    Sampler samplingMethod;
+    std::string name;
+
+    Metric(Sampler samplingMethod, std::string name) : samplingMethod(samplingMethod), name(name) {}
+    bool operator==(const Metric &rhs) const {
+        return samplingMethod == rhs.samplingMethod &&
+               name == rhs.name;
+    }
+
+    bool operator!=(const Metric &rhs) const {
+        return !(rhs == *this);
+    }
+};
+
+
+struct MetricHasher
+{
+  std::size_t operator()(const Metric& metric) const
+  {
+    using std::size_t;
+    using std::hash;
+    using std::string;
+    return hash<string>()(metric.name);
+  }
+};
+
 
 
 class Device {
     public:
-        Device(){
+        Device() {
 
         }
         std::vector<std::pair<Metric, double>> getData(bool isPolling = false);
-        virtual double fetchMetric(Metric metric) = 0;
+        virtual double fetchMetric(const int& metricnum) = 0;
         std::vector<Metric> getAllowedMetrics(){
             std::vector<Metric> result;
             for (auto pair : allowedMetrics) {
@@ -41,8 +71,7 @@ class Device {
             }
             return result;
         }
-        std::unordered_map<Metric, int> allowedMetrics;
-
+    std::unordered_map<Metric, int, MetricHasher> allowedMetrics;
 };   
 
 
@@ -50,8 +79,8 @@ class CPUPerf : public Device {
 public:
     CPUPerf() {
         //initialize allowed metrics with cycles only for now
-        allowedMetrics.insert(std::make_pair<Metric,int>(Metric(false, "cycles"),0));
-        allowedMetrics.insert(std::make_pair<Metric,int>(Metric(false, "instructions"),1));
+        allowedMetrics.insert(std::make_pair<Metric,int>(Metric(TWO_SHOT, "cycles"),0));
+        allowedMetrics.insert(std::make_pair<Metric,int>(Metric(TWO_SHOT, "instructions"),1));
 
     }
 
@@ -60,7 +89,7 @@ public:
         std::vector<std::pair<Metric, double>> result;
         //loop over each allowed metric and call fetchMetric on it
         for (auto& pair: allowedMetrics) {
-            result.push_back({pair.first, fetchMetric(pair.first)});
+            result.push_back({pair.first, fetchMetric(pair.second)});
         }
         return result;
         
@@ -74,49 +103,51 @@ public:
 
 
 
-
-    double fetchMetric(Metric metric) override {
-        if (metric.name == "cycles") {
-        struct perf_event_attr pe;
-            memset(&pe, 0, sizeof(struct perf_event_attr));
-            pe.type = static_cast<uint32_t>(PERF_TYPE_HARDWARE);
-            pe.size = sizeof(struct perf_event_attr);
-            pe.config = PERF_COUNT_HW_CPU_CYCLES;
-            pe.disabled = true;
-            pe.inherit = 1;
-            pe.inherit_stat = 0;
-            pe.exclude_user = !(0b111 & 0b1);
-            pe.exclude_kernel = !(0b111 & 0b10);
-            pe.exclude_hv = !(0b111 & 0b100);
-            pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
-            auto ans =syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
-            //also read our errno
-            if (errno != 0) {
-                std::cerr << "Error opening perf event: " << errno << std::endl;
+    double fetchMetric(const int& metricnum) override {
+        switch (metricnum) {
+            case 0: // cycles
+            {
+                struct perf_event_attr pe;
+                memset(&pe, 0, sizeof(struct perf_event_attr));
+                pe.type = static_cast<uint32_t>(PERF_TYPE_HARDWARE);
+                pe.size = sizeof(struct perf_event_attr);
+                pe.config = PERF_COUNT_HW_CPU_CYCLES;
+                pe.disabled = true;
+                pe.inherit = 1;
+                pe.inherit_stat = 0;
+                pe.exclude_user = !(0b111 & 0b1);
+                pe.exclude_kernel = !(0b111 & 0b10);
+                pe.exclude_hv = !(0b111 & 0b100);
+                pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
+                auto ans = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
+                if (errno != 0) {
+                    std::cerr << "Error opening perf event: " << errno << std::endl;
+                }
+                return ans;
             }
-            return ans;
-    }
-        if (metric.name == "instructions") {
-            struct perf_event_attr pe;
-            memset(&pe, 0, sizeof(struct perf_event_attr));
-            pe.type = static_cast<uint32_t>(PERF_TYPE_HARDWARE);
-            pe.size = sizeof(struct perf_event_attr);
-            pe.config = PERF_COUNT_HW_INSTRUCTIONS;
-            pe.disabled = true;
-            pe.inherit = 1;
-            pe.inherit_stat = 0;
-            pe.exclude_user = !(0b111 & 0b1);
-            pe.exclude_kernel = !(0b111 & 0b10);
-            pe.exclude_hv = !(0b111 & 0b100);
-            pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
-            auto ans =syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
-            //also read our errno
-            if (errno != 0) {
-                std::cerr << "Error opening perf event: " << errno << std::endl;
+            case 1: // instructions
+            {
+                struct perf_event_attr pe;
+                memset(&pe, 0, sizeof(struct perf_event_attr));
+                pe.type = static_cast<uint32_t>(PERF_TYPE_HARDWARE);
+                pe.size = sizeof(struct perf_event_attr);
+                pe.config = PERF_COUNT_HW_INSTRUCTIONS;
+                pe.disabled = true;
+                pe.inherit = 1;
+                pe.inherit_stat = 0;
+                pe.exclude_user = !(0b111 & 0b1);
+                pe.exclude_kernel = !(0b111 & 0b10);
+                pe.exclude_hv = !(0b111 & 0b100);
+                pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
+                auto ans = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
+                if (errno != 0) {
+                    std::cerr << "Error opening perf event: " << errno << std::endl;
+                }
+                return ans;
             }
-            return ans;
+            default:
+                return 0;
         }
-        return 0;
     }
     
     };
