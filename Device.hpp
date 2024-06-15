44284e4e266f98b9d672ad3601f739bf718ec3c0
fhilprec <fhilprec@googlemail.com>
2024-06-15 13:33:07 +0200

    a

--- Device.hpp
+++ Device.hpp
@@ -13,6 +13,9 @@
 #include <asm/unistd.h>
 #include <unistd.h>
 
+#define LOG(x) std::cout << x << std::endl 
+
+
 enum Sampler{
     ONE_SHOT,    // Reads at the end of the process
     TWO_SHOT,    // Reads both at the start and the end of the process and returns the difference
@@ -55,92 +58,115 @@ protected:
     std::unordered_map<Metric, int, MetricHasher> allowedMetrics;
 };
 
+
+
+
+
+
+
+
 class CPUPerf : public Device {
+private:
+
+    std::chrono::time_point<std::chrono::steady_clock> startTime;
+    std::chrono::time_point<std::chrono::steady_clock> stopTime;
+    enum EventDomain : uint8_t { USER = 0b1, KERNEL = 0b10, HYPERVISOR = 0b100, ALL = 0b111 };
+    struct event {
+      struct read_format {
+         uint64_t value;
+         uint64_t time_enabled;
+         uint64_t time_running;
+         uint64_t id;
+      };
+
+      perf_event_attr pe;
+      int fd;
+      read_format prev;
+      read_format data;
+
+      double readCounter() {
+         double multiplexingCorrection = static_cast<double>(data.time_enabled - prev.time_enabled) / static_cast<double>(data.time_running - prev.time_running);
+         return static_cast<double>(data.value - prev.value) * multiplexingCorrection;
+      }
+   };
+
+    std::vector<event> events;
+    std::vector<std::string> names;
+
+
 public:
-    enum MetricType {
-        CYCLES,
-        INSTRUCTIONS
-    };
-
-    struct read_format {
-        uint64_t value;
-        uint64_t time_enabled;
-        uint64_t time_running;
-        uint64_t id;
-    };
-
     CPUPerf() {
-        allowedMetrics.emplace(Metric(TWO_SHOT, "cycles"), CYCLES);
-        //allowedMetrics.emplace(Metric(TWO_SHOT, "instructions"), INSTRUCTIONS);
+        registerCounter("cycles", PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES);
+        registerCounter("kcycles", PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES, KERNEL);
+        registerCounter("instructions", PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS);
+        registerCounter("L1-misses", PERF_TYPE_HW_CACHE, PERF_COUNT_HW_CACHE_L1D|(PERF_COUNT_HW_CACHE_OP_READ<<8)|(PERF_COUNT_HW_CACHE_RESULT_MISS<<16));
+        registerCounter("LLC-misses", PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES);
+        registerCounter("branch-misses", PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES);
+        registerCounter("task-clock", PERF_TYPE_SOFTWARE, PERF_COUNT_SW_TASK_CLOCK);
+        registerCounter("task-clock", PERF_TYPE_SOFTWARE, PERF_COUNT_SW_TASK_CLOCK);
+
+
+        for (unsigned i=0; i<events.size(); i++) {
+         auto& event = events[i];
+         event.fd = static_cast<int>(syscall(__NR_perf_event_open, &event.pe, 0, -1, -1, 0));
+         if (event.fd < 0) {
+            std::cerr << "Error opening counter " << names[i] << std::endl;
+            events.resize(0);
+            names.resize(0);
+            return;
+         }
+      }
     }
 
+    void registerCounter(const std::string& name, uint64_t type, uint64_t eventID, EventDomain domain = ALL) {
+      names.push_back(name);
+      events.push_back(event());
+      auto& event = events.back();
+      auto& pe = event.pe;
+      memset(&pe, 0, sizeof(struct perf_event_attr));
+      pe.type = static_cast<uint32_t>(type);
+      pe.size = sizeof(struct perf_event_attr);
+      pe.config = eventID;
+      pe.disabled = true;
+      pe.inherit = 1;
+      pe.inherit_stat = 0;
+      pe.exclude_user = 0;
+      pe.exclude_kernel = 0;
+      pe.exclude_hv = 0;
+      pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
+   }
+
     void start() {
-        fillVector();
+        for (unsigned i=0; i<events.size(); i++) {
+         auto& event = events[i];
+         ioctl(event.fd, PERF_EVENT_IOC_RESET, 0);
+         ioctl(event.fd, PERF_EVENT_IOC_ENABLE, 0);
+         if (read(event.fd, &event.prev, sizeof(uint64_t) * 3) != sizeof(uint64_t) * 3)
+            std::cerr << "Error reading counter " << names[i] << std::endl;
+      }
+      startTime = std::chrono::steady_clock::now();
     }
 
     void stop() {
-        fillVector(false);
+       stopTime = std::chrono::steady_clock::now();
+      for (unsigned i=0; i<events.size(); i++) {
+         auto& event = events[i];
+         if (read(event.fd, &event.data, sizeof(uint64_t) * 3) != sizeof(uint64_t) * 3)
+            std::cerr << "Error reading counter " << names[i] << std::endl;
+         ioctl(event.fd, PERF_EVENT_IOC_DISABLE, 0);
+      }
     }
 
     std::vector<std::pair<Metric, double>> getData(bool isPolling = false) override {
         // TODO: Implement the actual data fetching based on polling if needed
         return {};
     }
-
+    
     void printVector() {
-        for (size_t i = 0; i < prev.size(); i++) {
-            std::cout << static_cast<double>(current[i].value - prev[i].value) << std::endl;
+        for (size_t i = 0; i < events.size(); i++) {
+            LOG(events[i].readCounter());
         }
     }
-
-private:
-    std::vector<read_format> prev;
-    std::vector<read_format> current;
-
-    void fillVector(bool first = true) {
-        if (first) {
-            for (const auto& pair : allowedMetrics) {
-                prev.push_back(fetchMetric(pair.second));
-            }
-        } else {
-            for (const auto& pair : allowedMetrics) {
-                current.push_back(fetchMetric(pair.second));
-            }
-        }
-    }
-
-    read_format fetchMetric(const int& metricNum) {
-        struct perf_event_attr pe;
-        memset(&pe, 0, sizeof(struct perf_event_attr));
-        pe.type = PERF_TYPE_HARDWARE;
-        pe.size = sizeof(struct perf_event_attr);
-        pe.config = (metricNum == CYCLES) ? PERF_COUNT_HW_CPU_CYCLES : PERF_COUNT_HW_INSTRUCTIONS;
-        pe.disabled = true;
-        pe.inherit = 1;
-        pe.exclude_user = 0;
-        pe.exclude_kernel = 0;
-        pe.exclude_hv = 0;
-        pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING;
-
-        struct read_format rf = {0};
-        int fd = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
-        if (fd == -1) {
-            std::cerr << "Error opening perf event" << std::endl;
-            return rf;
-        }
-
-        ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-        ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
-
-        if (read(fd, &rf, sizeof(rf)) == -1) {
-            std::cerr << "Error reading perf event" << std::endl;
-            close(fd);
-            return rf;
-        }
-
-        ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
-        close(fd);
-
-        return rf;
-    }
+    
+    
 };
--- test.cpp
+++ test.cpp
@@ -8,9 +8,10 @@
 int main(){
     //PerfEvent perf;
     CPUPerf cpu;
+    //perf.startCounters();
     cpu.start();
     int ans = 0;
-    //perf.startCounters();
+    
     for (int i = 0; i < 10000; i++) {
         ans = i + 6;
         ans = ans * ans * ans;
@@ -21,21 +22,3 @@ int main(){
     //perf.printReport(std::cout, 1);
 }
 
-/*
-int main(){
-    int a = 7;
-    int n = 10000;
-    PerfEvent e;
-    e.startCounters();
-    for (int i=0; i<n; i++) // this code will be measured
-    a = a * a;
-    e.stopCounters();
-    e.printReport(std::cout, n); / / use n as scale factor
-    std::cout << std::endl;
-
-    CSVWriter csv;
-    csv.newRow() << "this" << "is" << "the" << "first" << "row";
-    csv.newRow() << "this" << "is" << "the" << "second" << "row";
-    csv.writeToFile("foobar.csv");
-    return 0;
-}*/
