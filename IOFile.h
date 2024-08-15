b8524f1ddbd830cbed6568ec0948983007682893
fhilprec <florian@hilprecht.de>
2024-08-15 20:35:55 +0200

    init

---
+++ IOFile.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <unordered_map>
+#include "Device.hpp"
+
+class IOFile : public Device<IOFile>
+{
+private:
+    std::unordered_map<std::string, uint64_t> prevValues;
+    std::unordered_map<std::string, uint64_t> currentValues;
+    bool first = true;
+
+    void readIOStats();
+
+public:
+    IOFile();
+    ~IOFile() override = default;
+
+    std::vector<std::pair<Metric, Measurement>> getData(SamplingMethod sampler) override;
+    Measurement fetchMetric(const Metric& metric) override;
+    Measurement calculateMetric(const Metric& metric, const std::unordered_map<std::string, std::unordered_map<SamplingMethod, std::vector<std::vector<std::pair<Metric, Measurement>>>>>& requestedMetricsByDeviceBySamplingMethod) override;
+
+    static std::string getDeviceName();
+    static std::unordered_map<std::string, Metric> getAllDeviceMetricsByName();
+    static std::unordered_map<std::string, std::vector<Metric>> getNeededMetricsForCalculatedMetrics(const Metric& metric);
+};
