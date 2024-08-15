105563ea473f1daf869e34a31719a986db1fb0f3
fhilprec <florian@hilprecht.de>
2024-08-15 20:06:34 +0200

    terminate called after throwing an instance of 'std::out_of_range'

--- test.cpp
+++ test.cpp
@@ -1,54 +1,110 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <thread>
 #include "Device.hpp"
-#include "Counter.hpp"
 #include "CPUPerf.h"
+#include "IOFile.h"
 #include "Monitor.h"
-//sysctl -w kernel.perf_event_paranoid=-1
-//g++ test.cpp ; ./a.out ; rm a.out;
+
+// Function to perform some CPU-intensive work
+void doCPUWork() {
+    volatile int sum = 0;
+    for (int i = 0; i < 1000; ++i) {
+        sum += i;
+    }
+}
+
+// Function to write random data to a file
+void writeToFile(const std::string& filename, int sizeMB) {
+    std::ofstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "Failed to open file for writing: " << filename << std::endl;
+        return;
+    }
+
+    const int chunkSize = 1024 * 1024; // 1 MB
+    std::vector<char> buffer(chunkSize);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 255);
+
+    for (int i = 0; i < sizeMB; ++i) {
+        for (int j = 0; j < chunkSize; ++j) {
+            buffer[j] = static_cast<char>(dis(gen));
+        }
+        file.write(buffer.data(), chunkSize);
+
+        // Introduce a small delay to make the I/O activity more visible
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    file.close();
+}
+
+// Function to read data from a file
+void readFromFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "Failed to open file for reading: " << filename << std::endl;
+        return;
+    }
+
+    const int chunkSize = 1024 * 1024; // 1 MB
+    std::vector<char> buffer(chunkSize);
+
+    while (file) {
+        file.read(buffer.data(), chunkSize);
+        // Introduce a small delay to make the I/O activity more visible
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    }
+
+    file.close();
+}
 
 int main() {
-    /* CPUPerf cpu;
-     cpu.printVector(cpu.getData());
-     int i = 0;
-     for (int i = 0; i < 100; i++) {
-         i = i + 1;
-         i = i * i;
-     }
-     cpu.printVector(cpu.getData());*/
-
-
     std::vector<std::shared_ptr<IDevice>> devices;
-    auto* device = new CPUPerf();
+
+    auto* device = new IOFile();
     devices.emplace_back((IDevice*)device);
+    auto* device2 = new CPUPerf();
+    devices.emplace_back((IDevice*)device2);
+
     std::filesystem::path outputDirectory("testOutput");
     auto fullPath = absolute(outputDirectory);
-    std::chrono::milliseconds pollingTime = std::chrono::milliseconds(100);
 
+    // Create the Monitor object with the correct constructor signature
     Monitor monitor(devices, outputDirectory);
+
+    // Note: We can't set the polling time here as the constructor doesn't accept it.
+    // If you need to set a custom polling time, you might need to modify the Monitor class.
 
     monitor.start();
 
-    /*long long max=0;
-    int maxI=0;
-    for(int i=1;i<(1<<24);i++) {
-        long long ans = i;
-        long long steps = 0;
-        while (ans != 1) {
-            if (ans % 2 == 0) ans = ans / 2;
-            else ans = ans * 3 + 1;
-            steps += 1;
-        }
-        if(steps>max){
-            max=steps;
-            maxI = i;
-        }
-    }*/
-    int i = 0;
-    for (int i = 0; i < 100; i++) {
-        i = i + 1;
-        i = i * i;
-    }
-    //std::cout << "longest Collatz Path to 1: " << max << ", starting with number: " << maxI  <<std::endl;
-    monitor.stop();
-}
+    // Perform some CPU and I/O work
+    const std::string filename = "test_file.bin";
+    const int fileSizeMB = 50; // 50 MB file
 
+    std::cout << "Writing to file..." << std::endl;
+    writeToFile(filename, fileSizeMB);
+
+    std::cout << "Reading from file..." << std::endl;
+    readFromFile(filename);
+
+    std::cout << "Performing CPU work..." << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        doCPUWork();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    monitor.stop();
+
+    // Clean up the test file
+    std::filesystem::remove(filename);
+
+    std::cout << "Test completed. Check the testOutput directory for results." << std::endl;
+
+    return 0;
+}
