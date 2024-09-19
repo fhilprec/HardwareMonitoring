028b47277a433eb8e63b3a5a843791db114f3256
fhilprec <florian@hilprecht.de>
2024-09-19 17:25:48 +0200

    messed up redo NIC and CPUperf

--- Devices/Polling/CPUPerf.cpp
+++ Devices/Polling/CPUPerf.cpp
@@ -15,7 +15,12 @@
 
 static const std::vector METRICS{
     Metric(POLLING, "cycles", true),
-    Metric(POLLING, "L1-misses", true)
+    Metric(POLLING, "kcycles", true),
+    Metric(POLLING, "instructions", true),
+    Metric(POLLING, "L1-misses", true),
+    Metric(POLLING, "LLC-misses", true),
+    Metric(POLLING, "branch-missese", true),
+    Metric(POLLING, "task-clock", true)
 };
 
 CPUPerf::CPUPerf(const std::vector<Metric>& metricsToCount): Device(metricsToCount)
